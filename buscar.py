c50e3d37ad44efbfadce44071cd3e2f6fe3ddc45
Jorge Ulises Useche Cuellar <naturalmentejorge@gmail.com>
2018-07-16 18:24:32 -0500

    Add more dictionaries

--- buscar.py
+++ buscar.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 import re
 
-filename = 'listado-general.txt'
-length = 0
+#filename = 'libreoffice-dic-es_ANY.txt'
+filename = 'phpstorm-dict.txt'
+#filename = 'listado-general.txt'
+length = 0 # is for no limits
+#length = 7
 noletters = ['t']
 # politica
 # impactos
@@ -23,10 +26,13 @@ noletters = ['t']
 #regex = re.compile(r'^[a-zA-Z]{2}[oó][r].*$')
 #regex = re.compile(r'^[aá][a-z][aá][a-z]{2}[aá][a-z][ií].*$')
 #regex = re.compile(r'^[aá][a-z]{2}[oó][a-z]{4}[aá].*$')
-#regex = re.compile(r'^[f][a-z]{5}[a][n][t].*$')
+#regex = re.compile(r'^[f][a-záéíóúý]{4}[c][aá][n][t].*$')
+#regex = re.compile(r'^[a-záéíóúý][oó][l][aá][g][aá][s].*$')
+regex = re.compile(r'^[f][b].*$')
 #regex = re.compile(r'^[i][a-z]{3}[ií][a-z][a].*$')
 #regex = re.compile(r'^[i][a-záéíóúý][aá][a-záéíóúý][ií][a-záéíóúý][aá].*$')
-regex = re.compile(r'^[ií][a-záéíóúý]{4}[v][aá].*$')
+#regex = re.compile(r'^[ií][a-záéíóúý]{4}[v][aá].*$')
+#regex = re.compile(r'^[ií][a-záéíóúý]{4}[v][aá].*$')
 
 
 textfile = open(filename, 'r')
