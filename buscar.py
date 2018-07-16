bdd94386201823d3cfa656243c23a276e4e9de3e
Jorge Ulises Useche Cuellar <naturalmentejorge@gmail.com>
2018-07-16 09:28:14 -0500

    All source coude to search in dict

---
+++ buscar.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+import re
+
+filename = 'listado-general.txt'
+length = 0
+noletters = ['t']
+# politica
+# impactos
+# aspecto
+# inorganicos
+# organicos
+# adaptacion
+# aprovechables
+# internas (condiciones internas o del entorno)
+# desarrollo sostenible
+# economia sostenible
+# producción sostenible
+# consumo sostenible 
+# objetivos de desarrollo sostenible
+
+#regex = re.compile(r'^.?[o].*$')
+#regex = re.compile(r'^[a-zA-Z][oó].?[i][t][i][c].*$')
+#regex = re.compile(r'^[a-zA-Z]{2}[oó][r].*$')
+#regex = re.compile(r'^[aá][a-z][aá][a-z]{2}[aá][a-z][ií].*$')
+#regex = re.compile(r'^[aá][a-z]{2}[oó][a-z]{4}[aá].*$')
+#regex = re.compile(r'^[f][a-z]{5}[a][n][t].*$')
+#regex = re.compile(r'^[i][a-z]{3}[ií][a-z][a].*$')
+#regex = re.compile(r'^[i][a-záéíóúý][aá][a-záéíóúý][ií][a-záéíóúý][aá].*$')
+regex = re.compile(r'^[ií][a-záéíóúý]{4}[v][aá].*$')
+
+
+textfile = open(filename, 'r')
+matches = []
+for line in textfile:
+    if length > 0 and len(line) != length + 1:
+        continue
+    match = regex.findall(line)
+    for letter in noletters:
+        if str(match).find(letter) > -1:
+            continue
+    matches += match
+textfile.close()
+print(matches)
+print('longitud', length)
